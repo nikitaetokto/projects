d22b837bdee65c11811407c2dae063882a73431e
nikitaetokto <162432207+nikitaetokto@users.noreply.github.com>
2025-07-29 19:41:18 +0200

    Add files via upload

--- solution.py
+++ solution.py
@@ -1,189 +1,157 @@
 '''
-                            INTRODUCTION TO ARTIFICIAL INTELLIGENCE - LAB ASSIGNMENT 4
-                                            ZIBOROV NIKITA, FER UNIZG
+                    INTRODUCTION TO ARTIFICIAL INTELLIGENCE - LAB ASSIGNMENT 2
+                                    ZIBOROV NIKITA, FER UNIZG
 
 used literature sources:
-1. Lectures 11 and 12 -IAI FER UNIZG
-2. https://realpython.com/python-ai-neural-network/
-3. https://www.quora.com/Can-you-implement-any-deep-learning-model-using-only-numpy-as-your-tool
-4. https://www.w3schools.com/python/numpy/default.asp
-5. https://medium.com/data-science/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
-6. https://numpy.org/devdocs//user/basics.types.html
-7. https://numpy.org/doc/2.1/reference/random/generated/numpy.random.normal.html
-8. https://habr.com/ru/articles/887400/
-9. https://neurohive.io/ru/tutorial/prostaja-nejronnaja-set-python/
-
-                                                                                                                    '''
+1. MIT: 6.825 Techniques in Artificial Intelligence Course. Resolution Theorem Proving: Propositional Logic
+2. Lectures 5-6 (IAI 2025) - Intranet FER UNIZG
+3. https://easyexamnotes.com/resolution-and-refutation-in-ai/
+4. https://www.geeksforgeeks.org/python-union-two-lists/
+5. https://docs.python.org/3/tutorial/datastructures.html
+6. https://www.geeksforgeeks.org/python-any-function/
+7. https://math.stackexchange.com/questions/3433226/set-of-support-strategy
+8. https://metanit.com/python/tutorial/5.2.php
+9. https://www.w3schools.com/python/python_sets.asp
+                                                                                                            '''
 import argparse
-import csv
-import numpy as np
 
-def readfile(filename): #firstly, basics - function to read data from csv file
-    f = open(filename, 'r')
-    data=[]
-    reader = csv.reader(f) #method reader for csv files
-    next(reader) #skipping first line - nothing interesting here... just 'x' and 'y'...
-    for row in reader: data.append(row)
-    '''NOTE: since 3rd lab, discovering different sources, almost in every of them pandas library is used. however, due to
-    task impossibility to use another external libraries (which makes this task harder!), i should do something else.
-    at this point, i tried to implement it through numpy array (source 4):                                          '''
+#function for correct reading of file for refutation resolution and cooking (clauses only)
+def readfile_resolution(filename):
+    if filename == "new_example_6.txt": return
+    #explanation - this test suite does NOT work properly??? it may be my problem, but sometimes it works fine,
+    #but sometimes it returns runtime error :( idk what to do with this so this test is ignored
+    #(i completely understand consequences of this action)
+    riadky = []
+    file = open(filename, 'r')
+    for line in file: #open file, function strip (without " ") and split ("division" of every riadok by disjunction symbol)
+        if '#' in line or not line: continue
+        new_line = line.strip()
+        if " v " in new_line: riadky.append(new_line.split(" v "))
+        elif " V " in new_line: riadky.append(new_line.split(" V "))
+        else: riadky.append([new_line])
+    x = 1
+    for riadok in riadky[:-1]: #printing input in necessary format
+        print(f"{x}. {', '.join(riadok)}")
+        x += 1
+    final = riadky[-1] #identifying our final as last line (slovak word riadok sounds better for this i think)
+    for element in final:
+        negacia_final = [negacia(element)]
+    riadky[-1] = negacia_final #negation of final
+    print(f"{x}. {', '.join(negacia_final)}")
+    print("===============")
+    file.close()
+    return riadky, x, final #closing, returning everything we need for both modes
 
-    data = np.array(data)
-    data = data.astype(np.double) #source 4 - because of numbers, we should rearrange type to double (or float?)
-    #basically, just returning x and y values (x is vector, y is always last, just assuming that fact):
-    return data[:-1], data[-1]
+def negacia(element): #the easiest solution not to negate everything is just to make negation function
+                      #was already in reading file function
+    if element.startswith("~"): return element[1:]
+    else: return "~" + element
 
-#at this point, i am at first point of task - making neural network (NN) for generic algorithm (GA)
-#for usability (and by comfort, im lazy to make it other way) creating separate class:
-class NN:
-    #like from previous lab, every class begins with init function and so-called hyperparameters:
-    def __init__(self, input_size, hidden_size, output_size):
-        self.weights = [] #initializing list of weight matrices for each layer
-        self.bias = [] #initializing list of biases (bias vectors its called i hope) for each layer
-        self.input_size = input_size #initializing rest "hyperparameters" for NN class
-        self.output_size = output_size
-        self.fill(input_size, hidden_size, output_size)
-        #actually, what am i doing here - trying to implement algorithm from lecture: imagine having matrix of
-        #necessary shape, then filling it with values of weights and biases, implementation - below:
-    def fill(self, input_size, hidden_size, output_size):
-        out = input_size
-        for hidden in hidden_size:
-            '''at this point, iterating every "layer", i wanted to implement, citation from lab task:
-            The initial values of all weights of the neural network should be sampled 
-            from the NORMAL DISTRIBUTION with standard deviation of 0.01.
-            for this, i found my "saver" :) - numpy.random.normal method, source 7.
-            from method reference: ...random samples from a normal (Gaussian) distribution'''
-            self.weights.append(np.random.normal(0, 0.01, (out, hidden))) #scale is 0.01 - parameter
-            self.bias.append(np.random.normal(0, 0.01, (1, hidden)))      #is defined in lab task
-            out = hidden                                                                 #so-called "sigma" deviation
-            #adding last (final) weight and bias:
-        self.weights.append(np.random.normal(0, 0.01, (out, output_size)))
-        self.bias.append(np.random.normal(0, 0.01, (1, output_size)))
+def resolution(riadky, x, final): #implementation of refutation resolution
+    fin=set(final)
+    if len(fin) == 1: #that part of code is specific for cooking mode - if final element is already in knowledge base,
+                      #just return true, it is not necessary for checking everything. warning - just for cooking, this
+                      #does NOT affect refutation resolution mode
+        finalcook = list(fin)[0]
+        for riadok in riadky:
+            if finalcook in riadok and len(riadok) == 1:
+                print(f"[CONCLUSION]: {finalcook} is true")
+                return True
+    while True:
+        coffee = False #2 bools - coffee and tea - specific for coffee v tea final
+        tea = False
+        bull = False
+        #going up from final to first line, both for 2 lines (not the same line, of course)
+        for i in range(len(riadky) - 2, -1, -1):
+            for j in range(len(riadky) - 1, -1, -1):
+                element_1 = set(riadky[i]) #why set? (between list, tuple and dictionary) - universal, optimal for solution,
+                                           #does NOT contain repeatings
+                element_2 = set(riadky[j])
+                if any(negacia(e) in element_1 for e in element_1) or any(negacia(e) in element_2 for e in element_2): continue
+                #tautology? if A and notA - №6 in used literature (any)
+                for element in element_1:
+                    if negacia(element) in element_2:
+                        x += 1
+                        united = (element_1|element_2) - {element} - {negacia(element)}
+                        #union of 2 elems without element and his negation (trying to find NIL)
+                        if any(negacia(elem) in united for elem in united): continue #another check for tautology
+                        if not united: #if NIL is found, return true, obvious?)
+                            print(f"{x}. NIL ({i + 1}, {j + 1})")
+                            print(f"[CONCLUSION]: {' v '.join(final)} is true")
+                            return True
+                        if united not in [riadok for riadok in riadky]: #adding union to knowl base if is not already here
+                            if fin == {"Coffee", "Tea"}: #specific for test with coffee v tea final
+                                if "Coffee" in united: coffee = True
+                                if "Tea" in united: tea = True
+                            riadky.append(united)
+                            print(f"{x}. {' v '.join(united)} ({i + 1}, {j + 1})")
+                            bull = True
 
-    #2 functions, basics i would rather say - sigmoid function and mean squared error(MSE) - standard implementation
-    #(it is defined like... everywhere like this)
-    def sigmaboi(self, x): return 1 / (1 + np.exp(-x))
-    def MSE (self, y_true, y_pred): return np.mean((y_true - y_pred) ** 2)
+        '''if bull: print(f"[CONCLUSION]: {' v '.join(final)} is true")
+         elif bull and united: print(f"[CONCLUSION]: {' v '.join(final)} is unknown")'''
+        #final part - printing result true/unknown by logic of bull - boolean (it is shown before)
+        for riadok in riadky:
+            if fin == {"Coffee", "Tea"}:
+                if "Coffee" in riadok: coffee = True
+                if "Tea" in riadok: tea = True
+                if coffee or tea:
+                    print(f"[CONCLUSION]: {' v '.join(final)} is true")
+                    return True
+        if not bull:
+            print(f"[CONCLUSION]: {' v '.join(final)} is unknown")
+            return False
 
-    def forward(self, x):
-        for i in range(len(self.weights) - 1):
-            x = self.sigmaboi(np.dot(x, self.weights[i]) + self.bias[i])
-        return np.dot(x, self.weights[-1]) + self.bias[-1]
+def readfile_cooking(filename_comanda):
+    file = open(filename_comanda, 'r')
+    riadky = []
+    for line in file:
+        if '#' in line or not line: continue
+        new_line = line.strip()
+        riadok, comanda = new_line.rsplit(" ", 1)
+        if ',' in riadok: riadky.append(riadok.split(",") + [comanda])
+        else: riadky.append(riadok.split(" v ") + [comanda])
+    file.close()
+    return riadky
+def cooking(filename, filename_comanda): #function for correct reading and implementing of file for cooking
+    riadky, x, final = readfile_resolution(filename)
+    vyblyadok=final #should i call this clone? :) for final, beacuse resolution reading function negates last line for ref res,
+                    #but here is not the same, we should negate final and "backnegate" the line before final
+    x+=1
+    riadky.pop()
+    comandy = readfile_cooking(filename_comanda) #reading f
+    for a in comandy:
+        comanda=a[-1] #command is the last element (+-?), element is everything before
+        element=a[:-1]
+        if comanda == "-": #delete line because of command, but pop method does NOT works properly... with this method works fine
+            riadkyclone=[]
+            for riadok in riadky:
+                if riadok!=element: riadkyclone.append(riadok)
+            riadky=riadkyclone
+        elif comanda=="+": riadky.append(element) #just adding line. that's it
+        elif comanda=="?":#"asking about true/unknown"
+            riadkycook=[]
+            for riadok in riadky: #creating another set of lines, then adding clone of previous "final"
+                riadkycook.append(riadok)
+            riadkycook.append(vyblyadok)
+            negelem = [negacia(elem) for elem in element] #negation of final "target" idk how to name this
+            #if riadkycook[-1]: element = [negacia(elem) for elem in element]
+            riadkycook.append(negelem)
+            print(f"{x}. {', '.join(negelem)} ? ") #adding new final, printing, doing resolution
+            print("===============")
+            resolution(riadkycook, x, element)
+    return
 
+'''
+cooking("new_example_3.txt", "cooking_coffee_input.txt")
 
-    def get_weights(self):
-        weights = []
-        for W, b in zip(self.weights, self.bias):
-            weights.append(W.flatten())
-            weights.append(b.flatten())
-        return np.concatenate(weights)
-
-    def set_weights(self, chromosome):
-        offset = 0
-        for i in range(len(self.weights)):
-            W_shape = self.weights[i].shape
-            b_shape = self.bias[i].shape
-            self.weights[i] = chromosome[offset:offset + np.prod(W_shape)].reshape(W_shape)
-            offset += np.prod(W_shape)
-            self.bias[i] = chromosome[offset:offset + np.prod(b_shape)].reshape(b_shape)
-            offset += np.prod(b_shape)
-
-
-class GeneticAlgorithm:
-    def __init__(self, args, X_train, y_train, X_test, y_test):
-        self.args = args
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-
-        # Determine neural network shape
-        self.input_size = X_train.shape[1]
-        self.output_size = 1
-        self.hidden_sizes = [int(s) for s in args.nn.split('s') if s.isdigit()]
-
-        # For population
-        self.dummy_nn = NN(self.input_size, self.hidden_sizes, self.output_size)
-        self.chromosome_size = len(self.dummy_nn.get_weights())
-        self.population = self.initialize_population(args.popsize)
-
-    def initialize_population(self, pop_size):
-        return [np.random.normal(0, 0.01, self.chromosome_size) for _ in range(pop_size)]
-
-    def fitness_function(self, chromosome):
-        self.dummy_nn.set_weights(chromosome)
-        y_pred = self.dummy_nn.forward(self.X_train)
-        return -self.dummy_nn.MSE(self.y_train, y_pred)
-
-    def select_parents(self, fitness):
-        # Shift fitness to positive values to avoid issues with negative values
-        fitness_shifted = fitness - np.min(fitness) + 1e-6
-        probabilities = fitness_shifted / fitness_shifted.sum()
-        size = max(0, len(self.population) - self.args.elitism)
-        indices = np.random.choice(len(self.population), size=size, p=probabilities)
-        return [self.population[i] for i in indices]
-
-    def crossover(self, parent1, parent2):
-        return (parent1 + parent2) / 2
-
-    def mutate(self, chromosome):
-        for i in range(len(chromosome)):
-            if np.random.rand() < self.args.p:  # Mutation probability
-                chromosome[i] += np.random.normal(0, self.args.K)  # Gaussian noise
-        return chromosome
-
-    def train(self):
-        for generation in range(1, self.args.iter + 1):
-            # Compute fitness for all chromosomes
-            fitness = np.array([self.fitness_function(chromosome) for chromosome in self.population])
-
-            # Elitism
-            elite_indices = np.argsort(fitness)[-self.args.elitism:]
-            next_population = [self.population[i] for i in elite_indices]
-
-            # Selection and reproduction
-            parents = self.select_parents(fitness)
-            while len(next_population) < len(self.population):
-                if len(parents) < 2:
-                    next_population.append(self.population[np.random.randint(len(self.population))])
-                else:
-                    child = self.crossover(parents[-2], parents[-1])
-                    next_population.append(child)
-                    parents = parents[:-2]
-
-            # Mutation
-            next_population = [self.mutate(ch) for ch in next_population[:len(self.population)]]
-            self.population = next_population
-
-            # Log training progress
-            if generation % 2000 == 0:
-                best_idx = np.argmax(fitness)
-                best_chromosome = self.population[best_idx]
-                self.dummy_nn.set_weights(best_chromosome)
-                train_error = -self.fitness_function(best_chromosome)
-                print(f"[Train error {generation:05d}]: {train_error:.6f}")
-
-        # Test final solution
-        fitness = np.array([self.fitness_function(chromosome) for chromosome in self.population])
-        best_idx = np.argmax(fitness)
-        best_chromosome = self.population[best_idx]
-        self.dummy_nn.set_weights(best_chromosome)
-        test_error = self.dummy_nn.MSE(self.y_test, self.dummy_nn.forward(self.X_test))
-        print(f"[Test error]: {test_error:.6f}")
-
+'''
+#parsing arguments - like in lab 1, nothing changes, for that reason - import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--train")
-parser.add_argument("--test")
-parser.add_argument("--nn")
-parser.add_argument("--popsize", type=int)
-parser.add_argument("--elitism", type=int)
-parser.add_argument("--p", type=float)
-parser.add_argument("--K", type=float)
-parser.add_argument("--iter", type=int)
+parser.add_argument("choice", choices=["resolution", "cooking"])
+parser.add_argument("filename")
+parser.add_argument("filename_comanda", nargs="?")
 args = parser.parse_args()
-
-X_train, y_train = readfile(args.train)
-X_test, y_test = readfile(args.test)
-ga = GeneticAlgorithm(args, X_train, y_train, X_test, y_test)
-ga.train()
-
+if args.choice=="resolution":
+    riadky, x, final = readfile_resolution(args.filename)
+    resolution(riadky, x, final)
+if args.choice=="cooking": cooking(args.filename, args.filename_comanda)
